be9f2c3e7cd36d2f88937d693625382c66e04493
hesitationIsDefeat <onat@emrevivo.cmpe.boun.edu.tr>
2026-03-04 18:33:53 +0300

    doc(link): Add documentation for link.rs file

--- src/models/link.rs
+++ src/models/link.rs
@@ -1,16 +1,32 @@
+//! This file contains the required structures and relevant unit tests for the link of a network.
+
 #[derive(Debug, Clone, PartialEq)]
+/// Represents the type of a Link
 pub enum LinkType {
+    /// Represents physical connection through a cable
     ETHERNET,
+    /// Represents non-physical connection through air
     WIRELESS,
 }
-
+/// Represents node connections in the network topology
 pub struct Link {
+    /// Unique identifier
     pub id: usize,
+    /// Type (e.g. ETHERNET, WIRELESS)
     pub link_type: LinkType,
+    /// Maximum work capacity, the unit depends on the network type (e.g. Gbps, pps, tpm)
     pub capacity: f64,
 }
 
 impl Link {
+    /// Creates a Link with specific parameters.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use cmpe524_network_designer::models::link::{Link, LinkType};
+    /// let link = Link::new(0, LinkType::ETHERNET, 10.0);
+    /// ```
     pub fn new(id: usize, link_type: LinkType, capacity: f64) -> Self {
         Self {
             id,
