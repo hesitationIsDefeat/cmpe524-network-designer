6558bb5246a8a0d5c8b6f71487237e7a0f476d6e
hesitationIsDefeat <onat@emrevivo.cmpe.boun.edu.tr>
2026-03-04 17:31:47 +0300

    feat(link): Create link.rs file, add LinkType enum and Link struct

---
+++ src/models/link.rs
@@ -0,0 +1,16 @@
+pub enum LinkType {
+    ETHERNET,
+    WIRELESS,
+}
+
+pub struct Link {
+    pub id: usize,
+    pub link_type: LinkType,
+    pub capacity: f64,
+}
+
+impl Link {
+    pub fn new(id: usize, link_type: LinkType, capacity: f64) -> Self {
+        Self(id, link_type, capacity)
+    }
+}
