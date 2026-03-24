e4431d65d6b33b1c91c4c8c440568421be4b2c05
hesitationIsDefeat <onat@INV-NB-266.invent.inventanalytics.com>
2026-03-24 19:39:42 +0300

    feat(link): Add cost field to Link struct

--- src/models/link.rs
+++ src/models/link.rs
@@ -16,6 +16,8 @@ pub struct Link {
     pub link_type: LinkType,
     /// Maximum work capacity, the unit depends on the network type (e.g. Gbps, pps, tpm)
     pub capacity: f64,
+    /// Cost of using a Link
+    pub cost: f64,
 }
 
 impl Link {
@@ -25,13 +27,14 @@ impl Link {
     ///
     /// ```
     /// use cmpe524_network_designer::models::link::{Link, LinkType};
-    /// let link = Link::new(0, LinkType::ETHERNET, 10.0);
+    /// let link = Link::new(0, LinkType::ETHERNET, 10.0, 5.0);
     /// ```
-    pub fn new(id: usize, link_type: LinkType, capacity: f64) -> Self {
+    pub fn new(id: usize, link_type: LinkType, capacity: f64, cost: f64) -> Self {
         Self {
             id,
             link_type,
             capacity,
+            cost,
         }
     }
 }
@@ -42,20 +45,22 @@ mod tests {
 
     #[test]
     pub fn test_link_creation() {
-        let link: Link = Link::new(0, LinkType::ETHERNET, 10.0);
+        let link: Link = Link::new(0, LinkType::ETHERNET, 10.0, 5.0);
 
         assert_eq!(link.id, 0);
         assert_eq!(link.link_type, LinkType::ETHERNET);
         assert_eq!(link.capacity, 10.0);
+        assert_eq!(link.cost, 5.0)
     }
 
     #[test]
     pub fn test_link_comparison() {
-        let first_link: Link = Link::new(0, LinkType::ETHERNET, 10.0);
-        let second_link: Link = Link::new(1, LinkType::WIRELESS, 15.0);
+        let first_link: Link = Link::new(0, LinkType::ETHERNET, 10.0, 5.0);
+        let second_link: Link = Link::new(1, LinkType::WIRELESS, 15.0, 6.0);
 
         assert!(first_link.id < second_link.id);
         assert_ne!(first_link.link_type, second_link.link_type);
         assert!(first_link.capacity < second_link.capacity);
+        assert!(first_link.cost < second_link.cost)
     }
 }
