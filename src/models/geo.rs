466bbf70fa05b94668a80dc5fca50a07afd99cd5
hesitationIsDefeat <onat@INV-NB-266.invent.inventanalytics.com>
2026-03-26 10:02:14 +0300

    mov(geo): Move Point3D struct to geo.rs file

--- src/models/geo.rs
+++ src/models/geo.rs
@@ -1,1 +1,11 @@
 //! This file contains utility code required to handle geographic functionality.
+
+/// Represents a point or a vector
+pub struct Point3D {
+    /// X-axis value
+    pub x: f64,
+    /// Y-axis value
+    pub y: f64,
+    /// Z-axis value
+    pub z: f64,
+}
