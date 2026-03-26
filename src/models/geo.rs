b33b9c0524ab5046617bb6d6d35f2259e51dabbd
hesitationIsDefeat <onat@INV-NB-266.invent.inventanalytics.com>
2026-03-26 16:58:46 +0300

    upd(geo): Update traits of Point3D struct

--- src/models/geo.rs
+++ src/models/geo.rs
@@ -1,6 +1,7 @@
 //! This file contains utility code required to handle geographic functionality.
 
 /// Represents a point or a vector
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point3D {
     /// X-axis value
     pub x: f64,
