1d8bbb463b84475b13d4b7e3c355c818ea4d94f3
hesitationIsDefeat <onat@Bayram.cmpe.boun.edu.tr>
2026-04-02 18:11:18 +0300

    feat(placement): Create PlacementStrategy trait for working with multiple placement algorithms

--- src/algorithms/placement.rs
+++ src/algorithms/placement.rs
@@ -1,1 +1,10 @@
-
+/// The common interface for all UAV placement algorithms.
+pub trait PlacementStrategy {
+    /// Calculates the optimal starting positions for a set of UAVs.
+    fn generate_positions(
+        &self,
+        uav_count: usize,
+        users: &[Point3D],
+        bounds: &Bounds3D,
+    ) -> Vec<Point3D>;
+}
