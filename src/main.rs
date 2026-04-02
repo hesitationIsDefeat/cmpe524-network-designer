0c9983a5f92cb7d9bbd2e978f88eb975555a3321
hesitationIsDefeat <onat@Bayram.cmpe.boun.edu.tr>
2026-04-02 16:42:53 +0300

    feat(main): Add initial user locations config parsing code in main

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ fn main() {
 
     let system_params = config::load_system_params("config/system_params.toml");
     let constraints = config::load_constraints("config/constraints.toml");
+    let initial_users = config::load_initial_users("config/initial_users.json");
 
     println!("✅ Loaded {} UAVs.", system_params.uav_amount);
     println!(
         "✅ Target Success Rate: {}%",
         constraints.min_overall_success_rate * 100.0
     );
+    println!("✅ Loaded {} Users.", initial_users.len());
 }
