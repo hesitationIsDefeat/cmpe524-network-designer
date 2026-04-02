a5169d45e086e19464a3d0c80a8f97d2fb25ac23
hesitationIsDefeat <onat@Bayram.cmpe.boun.edu.tr>
2026-04-02 16:40:09 +0300

    feat(main): Add constraints config parsing code in main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ fn main() {
     println!("Loading Configuration...");
 
     let system_params = config::load_system_params("config/system_params.toml");
+    let constraints = config::load_constraints("config/constraints.toml");
 
     println!("✅ Loaded {} UAVs.", system_params.uav_amount);
+    println!(
+        "✅ Target Success Rate: {}%",
+        constraints.min_overall_success_rate * 100.0
+    );
 }
