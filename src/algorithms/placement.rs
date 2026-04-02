0680aa6fc091ee92669db46c43b09c51f38863a7
hesitationIsDefeat <onat@Bayram.cmpe.boun.edu.tr>
2026-04-02 18:15:41 +0300

    feat(placement): Create KMeansPlacement struct

--- src/algorithms/placement.rs
+++ src/algorithms/placement.rs
@@ -10,3 +10,12 @@ pub trait PlacementStrategy {
         bounds: &Bounds3D,
     ) -> Vec<Point3D>;
 }
+
+/// A placement strategy that uses K-Means clustering to group users
+/// and places a UAV at the geographic center of each cluster.
+pub struct KMeansPlacement {
+    /// The maximum number of times the algorithm will adjust positions before giving up.
+    pub max_iterations: usize,
+    /// The default altitude (Z-axis) to set for the UAVs once X and Y are found.
+    pub target_altitude: f64,
+}
