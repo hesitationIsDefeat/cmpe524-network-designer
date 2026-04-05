42fbd45c8651a95c1ba4136ef3b50cd0d57df68d
hesitationIsDefeat <onat@192.168.1.12>
2026-04-05 19:30:52 +0300

    feat(main): Add initial UAV location handling to main
    Closes #9

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 pub mod config;
+use cmpe524_network_designer::algorithms::placement::KMeansPlacement;
+use cmpe524_network_designer::algorithms::placement::PlacementStrategy;
+use cmpe524_network_designer::models::geo::Bounds3D;
+use cmpe524_network_designer::models::geo::Point3D;
 
 fn main() {
     println!("Loading Configuration...");
@@ -13,4 +17,39 @@ fn main() {
         constraints.min_overall_success_rate * 100.0
     );
     println!("✅ Loaded {} Users.", initial_users.len());
+
+    let user_points: Vec<Point3D> = initial_users
+        .iter()
+        .map(|u| Point3D {
+            x: u.x,
+            y: u.y,
+            z: 0.0,
+        })
+        .collect();
+
+    let bounds: Bounds3D = system_params.area.to_bounds3d();
+
+    let strategy = KMeansPlacement {
+        max_iterations: 100,
+        target_altitude: 50.0, // UAVs will hover at Z = 50
+    };
+
+    println!("\n⚙️  Calculating optimal UAV placement via K-Means...");
+    let uav_starting_positions = strategy.generate_positions(
+        system_params.uav_amount,
+        &user_points,
+        &bounds, // Need to map this to Bounds3D first
+    );
+
+    println!("\n🎯 Final UAV Starting Positions:");
+    for (index, pos) in uav_starting_positions.iter().enumerate() {
+        // Using {:.2} to format the floating point numbers to 2 decimal places for readability
+        println!(
+            "   UAV [{:02}]: X = {:>7.2}, Y = {:>7.2}, Z = {:>5.2}",
+            index + 1,
+            pos.x,
+            pos.y,
+            pos.z
+        );
+    }
 }
